f5164f249cad1786f53957fd3a3f9628c0576e08
Pedro Henrique Silva Freire <psilvafreire@gmail.com>
2026-06-07 21:49:27 -0300

    mais um pra dar sorte

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,30 @@ import authRoutes from './routes/authRoutes';
 import taskRoutes from './routes/taskRoutes';
 
 const app = express();
+const allowedOrigins = new Set([
+  process.env.FRONTEND_ORIGIN,
+  'https://web-front-livid.vercel.app',
+  'http://localhost:4200',
+  'http://localhost:3000',
+  'http://localhost:3333'
+].filter((origin): origin is string => Boolean(origin)));
 
-app.use(cors());
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.has(origin)) {
+      callback(null, true);
+      return;
+    }
+
+    callback(null, false);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
